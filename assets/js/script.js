5d6da94a77f3bf6fff0827e2c1c2343b67fb475f
hasnurzen <hasnurzen@gmail.com>
2025-07-13 20:19:26 +0700

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function showMessage() {
-  const name = document.getElementById("name").value;
-  document.getElementById("hasil").innerText =
-    "Terima kasih, " + name + ", telah mendaftar!";
-  return false;
-}
